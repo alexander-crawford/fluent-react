22753a8ff0bb1de4e644badd21dc3567e06fb411
Alexander Crawford <mail@alexander-crawford.com>
2025-10-28 13:21:26 +0100

    Adjust layout of experience page

--- porfolio/app/components/posts.tsx
+++ porfolio/app/components/posts.tsx
@@ -22,7 +22,7 @@ export function BlogPosts() {
             href={`/blog/${post.slug}`}
           >
             <div className="w-full flex flex-col md:flex-row space-x-0 md:space-x-2">
-              <p className="text-neutral-600 dark:text-neutral-400 w-[100px] tabular-nums">
+              <p className="text-neutral-600 dark:text-neutral-400 w-fit tabular-nums">
                 {formatDate(post.metadata.publishedAt, false)}
               </p>
               <p className="text-neutral-900 dark:text-neutral-100 tracking-tight">
@@ -56,16 +56,18 @@ export function ExperiencePosts() {
             href={`/experience/${post.slug}`}
           >
             <div className="w-full flex flex-col md:flex-row space-x-0 md:space-x-2">
-              <p className="text-neutral-600 dark:text-neutral-400 w-[200px] tabular-nums">
+              <p className="text-neutral-600 dark:text-neutral-400 w-fit tabular-nums">
                 <span>{formatDate(post.metadata.started, false, true)}</span>
                 <span> - </span>
                 <span>{formatDate(post.metadata.ended, false, true)}</span>
               </p>
-              <p className="text-lg text-neutral-900 dark:text-neutral-100 tracking-tight">
-                {post.metadata.title}
-              </p>
-              <p className="text-neutral-900 dark:text-neutral-100 tracking-tight">
-                {post.metadata.company}
+              <p>
+                  <p className="text-neutral-900 dark:text-neutral-100 tracking-tight">
+                    {post.metadata.title}
+                  </p>
+                  <p className="text-neutral-900 dark:text-neutral-100 tracking-tight">
+                    {post.metadata.company}
+                  </p>
               </p>
             </div>
           </Link>
