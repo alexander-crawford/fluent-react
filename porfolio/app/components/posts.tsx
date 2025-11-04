9faea037e38d043dfc27a053e683aeba1f30ec42
Alexander Crawford <mail@alexander-crawford.com>
2025-11-04 13:03:24 +0100

    Adjust experience page layout

--- porfolio/app/components/posts.tsx
+++ porfolio/app/components/posts.tsx
@@ -57,20 +57,18 @@ export function ExperiencePosts() {
         })
         .map((post) => (
           <div className="flex flex-col space-y-1 mb-4" >
-            <div className="w-full flex flex-col md:flex-row space-x-0 md:space-x-2">
-              <p className="text-neutral-600 dark:text-neutral-400 w-fit tabular-nums">
-                <span>{formatDate(post.metadata.started, false, true)}</span>
-                <span> - </span>
-                <span>{formatDate(post.metadata.ended, false, true)}</span>
-              </p>
-              <p className="text-neutral-900 dark:text-neutral-100 tracking-tight">
-                <span>{post.metadata.title}</span>
-                <span> - </span>
-                <span>{post.metadata.company}</span>
-                <LinkIcon href={post.metadata.link} />
-              </p>
-              <TechList tech={post.metadata.tech} />
-            </div>
+            <p className="text-neutral-600 dark:text-neutral-400 w-fit tabular-nums">
+              <span>{formatDate(post.metadata.started, false, true)}</span>
+              <span> - </span>
+              <span>{formatDate(post.metadata.ended, false, true)}</span>
+            </p>
+            <p className="text-neutral-900 dark:text-neutral-100 tracking-tight">
+              <span>{post.metadata.title}</span>
+              <span> - </span>
+              <span>{post.metadata.company}</span>
+              <LinkIcon href={post.metadata.link} />
+            </p>
+            <TechList tech={post.metadata.tech} />
             <p className="text-neutral-900 dark:text-neutral-100 tracking-tight">
               {post.metadata.summary}
             </p>
